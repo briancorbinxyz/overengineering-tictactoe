8e65ecab51fef51df8eff623e136d306b3ffbd86
Brian Corbin <me@briancorbin.co.uk>
2024-07-21 12:01:52 -0400

    JDK21: Virtual Threads - Refactor (move net address resolution to test code and track some stats)

--- app/src/main/java/org/example/GameServer.java
+++ app/src/main/java/org/example/GameServer.java
@@ -5,8 +5,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.LongAccumulator;
+import java.util.concurrent.atomic.LongAdder;
 
 public class GameServer {
+
+    private final LongAdder concurrentGames = new LongAdder();
+
+    private final LongAccumulator maxGames = new LongAccumulator(Long::max, 0);
+
+    private final LongAccumulator totalGames = new LongAccumulator(Long::sum, 0);
 
     public static void main(String[] args) throws Exception {
         GameServer server = new GameServer();
@@ -21,6 +29,10 @@ public class GameServer {
         } catch (Exception e) {
             System.out.println(e);
             throw new RuntimeException(e);
+        } finally {
+            System.out.println("Server shutting down.");
+            System.out.println("Total games played: " + server.totalGames.get());
+            System.out.println("Maximum number of concurrent games: " + server.maxGames.get());
         }
     }
 
@@ -33,11 +45,15 @@ public class GameServer {
                     var playerX = new ClientServerBotPlayer("X", socketPlayerOne);
                     var playerO = new ClientServerBotPlayer("O", socketPlayerTwo)
                 ) {
+                    concurrentGames.increment();
+                    System.out.println(concurrentGames.longValue() + " games in progress.");
                     Game game = new Game(3, playerX, playerO);
                     game.play();
                 } catch (Exception e) {
                     System.out.println(e);
                     throw new RuntimeException(e);
+                } finally {
+                    concurrentGames.decrement();
                 }
             });
         }
