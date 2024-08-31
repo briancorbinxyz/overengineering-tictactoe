c1d3ed6db7ebb0ca0215ce32b484a34c442c2711
Brian Corbin <me@briancorbin.co.uk>
2024-08-31 18:54:10 -0400

    fix: under certain jvm vendors the Kyber security may fail due to jar signing. Switching to the version used by GitHub until signing is configured correctly

--- api/src/test/java/org/xxdc/oss/example/security/KyberKEMSpiTest.java
+++ api/src/test/java/org/xxdc/oss/example/security/KyberKEMSpiTest.java
@@ -10,17 +10,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
- * Tests the functionality of the KyberKEMSpi class, which is an implementation of the
- * Kyber Key Encapsulation Mechanism (KEM) algorithm.
+ * Tests the functionality of the KyberKEMSpi class, which is an implementation of the Kyber Key
+ * Encapsulation Mechanism (KEM) algorithm.
  *
- * The tests cover the following scenarios:
- * - Verifying that a new instance of KyberKEMSpi can be created successfully.
- * - Verifying that the Kyber KEM algorithm can be loaded and used through the
- *   Java Security API.
- * 
- * Note: This may fail if the Kyber KEM algorithm is not registered with the Java Security API due
- * to java security provider issues and or if the jar file has not been signed with the
- * correct key. The Temurin JDK does not worry about this.
+ * <p>The tests cover the following scenarios: - Verifying that a new instance of KyberKEMSpi can be
+ * created successfully. - Verifying that the Kyber KEM algorithm can be loaded and used through the
+ * Java Security API.
+ *
+ * <p>Note: This may fail if the Kyber KEM algorithm is not registered with the Java Security API
+ * due to java security provider issues and or if the jar file has not been signed with the correct
+ * key. The Temurin JDK does not worry about this.
  */
 public class KyberKEMSpiTest {
 
