1ec24926110bd5514c808f8b8169b1a4161c3027
Brian Corbin <me@briancorbin.co.uk>
2024-08-31 22:35:35 -0400

    refactor: refactor the security and messaging layer into tcp-gameserver

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -18,7 +18,7 @@ val libSuffix = when {
 dependencies {
     // Native Library (Rust)
     implementation(project(":native"))
-    testRuntimeOnly("org.xxdc.oss.example:tictactoe-native-$libSuffix:1.0.0")
+    testRuntimeOnly("org.xxdc.oss.example:tictactoe-native-$libSuffix:1.1.0")
 
 
     // JDK9: Platform Logging (Third-Party)
