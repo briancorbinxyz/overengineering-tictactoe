0dba4611645e92a18d44a4dae74f180de1efd0d8
Brian Corbin <me@briancorbin.co.uk>
2025-04-12 02:11:21 -0400

    chore: bump version for maven central publishing

--- buildSrc/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
@@ -70,7 +70,7 @@ testing {
     }
 }
 
-val projectVersion by extra("2.0.4")
+val projectVersion by extra("2.0.5")
 
 public val jdkVersion = 24
 // Apply a specific Java toolchain to ease working on different environments.
